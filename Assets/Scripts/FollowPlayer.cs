7b790cc9cd85422b970ba66cc6aeeb9e06bd3fd9
Louis <lthorpe-monaghan1@sheffield.ac.uk>
2020-07-29 15:49:31 +0100

    Added camera rotation that orbits the player if you rotate them

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -8,16 +8,17 @@ public class FollowPlayer : MonoBehaviour
 	private Vector3 velocity = Vector3.zero;
 	public Transform target;
 	public Vector3 offset;
+	public float testoffset;
 
 	// Update is called once per frame
 	void FixedUpdate()
 	{
 		if (target)
 		{
-			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(offset.x, offset.y, offset.z));
-			Vector3 destination = transform.position + delta;
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+			Vector3 delta = target.position + (target.forward*offset.x) + (target.up * offset.y) + (target.right*offset.z);
+			transform.position = Vector3.Lerp(transform.position, delta, dampTime);
+			Vector3 rotation = target.rotation.eulerAngles + new Vector3(0f, -90f, 0f);
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotation),dampTime);
 		}
 
 	}
